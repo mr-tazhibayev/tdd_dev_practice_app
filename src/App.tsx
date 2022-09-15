274c430f33012707dc6b65391c45bb5c810af0ee
adikweb <adilhan97kz@gmail.com>
2022-09-16 03:11:26 +0600

    shit-code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <h1>tdd - игра</h1>
+      <Counter initialValue={0}/>
     </div>
   );
 }
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,10 +2,26 @@ import React, { useState } from 'react'
 
 export interface CounterProps {
     initialValue?: number;
+    initialTick?: number;
 }
 
-const Counter:React.FC<CounterProps> = (props) => {
-    return <></>
+const Counter: React.FC<CounterProps> = (props) => {
+    const [initialValue, setInitialValue] = useState<number>(props.initialValue || 0);
+    const [tick] = useState<number>(props.initialTick||1);
+    const minMax = (value: number): number => Math.max(0, Math.min(value, value + tick));
+
+    return <>
+         <h3>{initialValue}</h3>
+
+        <button onClick={() => setInitialValue(minMax(initialValue - tick))}>
+            Уменьшить
+        </button>
+
+        <button onClick={()=>setInitialValue(minMax(initialValue+tick))}>
+            Увеличить
+        </button>
+
+    </>
 }
 
 export default Counter
